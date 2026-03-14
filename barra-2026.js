b2fc79291882863edd1656285bf9435b8024e6db
glivio21 <glivio18@gmail.com>
2026-03-14 05:03:09 -0300

    Update barra-2026.js

--- barra-2026.js
+++ barra-2026.js
@@ -196,58 +196,7 @@ padrao:"https://icons.iconarchive.com/icons/be-os/be-box/32/Be-IDE-icon.png"
     {
       titulo: '🤝 Tribo',
       icone: "https://icons.iconarchive.com/icons/iconfactory/sketchcons/32/smiley-icon.png",
-      scripts: [
-        ["📊 Noblagens TWStats","https://icons.iconarchive.com/icons/iconfactory/sketchcons/32/smiley-icon.png",function(){ var api = "https://api.codetabs.com/v1/proxy?quest=";
-var twstats = "/index.php?page=ennoblements&live=live";
-var url = api + game_data.world + twstats;
-
-var indexvillage = "index.php?page=village&id=";
-var indexplayer = "index.php?page=player&id=";
-var indextribe = "index.php?page=tribe&id=";
-
-var screenvillage = "&screen=info_village&id=";
-var screenplayer = "&screen=info_player&id=";
-var screenally = "&screen=info_ally&id=";
-
-var game = "/game.php?village=";
-
-$.get(url,function(data){
-
-if($(data).find("tr").hasClass("r1")){
-table=$(data).find(".widget").eq(1).find("tr");
-}else{
-table="Neste servidor não há nobres ao vivo.";
-}
-
-Dialog.show("foglalasok",function(container){
-
-$(`<table class="vis" id="table"><tbody></tbody></table>`).appendTo(container);
-
-$(table).appendTo("#table > tbody");
-
-});
-
-$("#table").find("a").each(function(key,val){
-
-var value=$(val).attr("href");
-
-if($(val).is(`[href*="${indexvillage}"]`)){
-$(val).attr("href",value.replace(indexvillage,game+game_data.village.id+screenvillage));
-}
-
-if($(val).is(`[href*="${indexplayer}"]`)){
-$(val).attr("href",value.replace(indexplayer,game+game_data.village.id+screenplayer));
-}
-
-if($(val).is(`[href*="${indextribe}"]`)){
-$(val).attr("href",value.replace(indextribe,game+game_data.village.id+screenally));
-}
-
-});
-
-});
-
-}] }],
+      scripts: [       
         ["Aristocracia", "https://icons.iconarchive.com/icons/iconfactory/sketchcons/32/smiley-icon.png", "https://shinko-to-kuma.com/scripts/overwatch.js"],
         ["Ver ataques na tribo", "https://icons.iconarchive.com/icons/iconfactory/sketchcons/32/smiley-icon.png", "https://dl.dropboxusercontent.com/s/ikunxd5d59059b4/scriptMostrarAtaquesACaminho.js"],
         ["Ataques (Tribo) - Membros", "https://icons.iconarchive.com/icons/iconfactory/sketchcons/32/smiley-icon.png", "https://dl.dropboxusercontent.com/s/oy16zihcrmtul4k/tribeinc.js"],
